54849453ba9de937968679644c55d2b46b674d19
tetsuya-hasegawa <hase1123v@gmail.com>
2020-07-03 11:00:45 +0000

    シングルページの行間を空けた

--- resources/views/profile/single.blade.php
+++ resources/views/profile/single.blade.php
@@ -6,14 +6,18 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>バイク記事</h2>
+                <br>
                 <h3>タイトル</h3>   
                 <div>{{ $profile_form->title }}</div>
                 <div>{{ $profile_form->body }}</div>
                       
+                    <br>  
+                    
                     <div class="caption mx-auto">
                          <img src="{{ $profile_form->image_path }}" width="100%" height="100%">
                     </div>
                     
+                    <br>
                     
                     <div class="col-md-4">
                      <a href="{{ action('ProfileController@index') }}" role="button" class="btn btn-primary">トップページへ戻る</a>
