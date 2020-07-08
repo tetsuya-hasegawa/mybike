35125d949164f71b1c8acf4d964759ba5a708ca8
tetsuya-hasegawa <hase1123v@gmail.com>
2020-07-08 08:47:52 +0000

    ナビバーをハンバーガーにしてみた

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -42,13 +42,35 @@
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
                             
-                            <!-- ここにナビボタンを追記 -->
+                            <!-- ここにナビボタンを追記-->
+                            <nav class="navbar navbar-dark bg-dark">
+  <a href="#" class="navbar-brand">Navbar</a>
+  <button class="navbar-toggler" type="button"
+      data-toggle="collapse"
+      data-target="#navmenu1"
+      aria-controls="navmenu1"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navmenu1">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="{{ action('NavbarController@add') }}">プロフィール</a>
+      <a class="nav-item nav-link" href="{{ action('NavbarController@add2') }}">ポートフォリオの紹介</a>
+      <a class="nav-item nav-link" href="{{ action('NavbarController@add3') }}">このブログについて</a>
+    </div>
+  </div>
+</nav>
+                            
+                            
+                            
+                         <!-- ここにナビボタンを追記
                           <a href="{{ action('NavbarController@add') }}" role="button" class="btn btn-primary">プロフィール</a>
                           <br><br>
                           <a href="{{ action('NavbarController@add2') }}" role="button" class="btn btn-primary">ポートフォリオの紹介</a></a>
                           <br><br>
                           <a href="{{ action('NavbarController@add3') }}" role="button" class="btn btn-primary">このブログについて</a>
-                          
+                          -->
                         </ul>
 
                          <!-- Right Side Of Navbar -->
