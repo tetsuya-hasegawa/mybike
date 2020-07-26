1b5830df2067ce8de6ddc532be6a73aa23a9be8b
tetsuya-hasegawa <hase1123v@gmail.com>
2020-07-26 08:39:52 +0000

    Navbar変更-2

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -44,14 +44,7 @@
                             
                             <!-- ここにナビボタンを追記-->
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <a href="#" class="navbar-brand">このポートフォリオについて</a>
-  <div class="collapse navbar-collapse" id="navmenu1">
-  <div class="navbar-nav">
- 　　 <a class="nav-item nav-link" href="{{ action('NavbarController@myprofile') }}">プロフィール</a>
-　　　<a class="nav-item nav-link" href="{{ action('NavbarController@myportfolio') }}">ポートフォリオ機能紹介</a>
-　　　<a class="nav-item nav-link" href="{{ action('NavbarController@myblog') }}">これまでの学習について</a>
-　</div>
-　</div>
+  <a href="#" class="navbar-brand">Navbar</a>
   <button class="navbar-toggler" type="button"
       data-toggle="collapse"
       data-target="#navmenu1"
@@ -60,13 +53,13 @@
       aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <!-- <div class="collapse navbar-collapse" id="navmenu1">
+  <div class="collapse navbar-collapse" id="navmenu1">
     <div class="navbar-nav">
       <a class="nav-item nav-link" href="{{ action('NavbarController@myprofile') }}">プロフィール</a>
-      <a class="nav-item nav-link" href="{{ action('NavbarController@myportfolio') }}">ポートフォリオ機能紹介</a>
+      <a class="nav-item nav-link" href="{{ action('NavbarController@myportfolio') }}">ポートフォリオの紹介</a>
       <a class="nav-item nav-link" href="{{ action('NavbarController@myblog') }}">これまでの学習について</a>
     </div>
-  </div>-->
+  </div>
 </nav>
                             
                             
